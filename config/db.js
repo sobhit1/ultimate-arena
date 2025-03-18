ac7feb319534df78bf475b5c206a9ba5a0025f90
sobhit1 <sobhitraghav1092@gmail.com>
2025-03-18 19:31:02 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import mysql from 'mysql2';
+import 'dotenv/config'
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+export default pool;
